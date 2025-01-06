64dc98653956348a65f9177e4ba44b907bc7ebe2
jormal <jormal@naver.com>
2025-01-07 00:53:05 +0900

    Add tests for dependency files
    - Implement tests to verify the existence of requirements files
    - Ensure requirements are in alphabetical order
    - Verify essential dependencies are included in the requirements
    - Check optional dependencies are listed in the corresponding requirements files
    
    Signed-off-by: jormal <jormal@naver.com>

--- tests/dependencies/test_dependencies.py
+++ tests/dependencies/test_dependencies.py
@@ -1,6 +1,85 @@
-class TestDependencies:
-    """Tests the dependency."""
+from football_data_puller.utils.constants import PWD
+from tests.dependencies.utils import (
+    read_requirements_paths,
+    read_dependencies,
+    read_essential_packages,
+    read_requirements,
+    read_optional_dependencies_keys,
+    read_requirements_from_path,
+)
 
-    def test_dummy(self) -> None:
-        """Dummy test."""
-        assert True
+
+class TestDependencies:
+    """
+    Tests the dependency status.
+    """
+
+    optional_package_paths = {
+        "all": PWD,
+        "dev": PWD / "football_data_puller",
+        "test": PWD / "tests",
+    }
+
+    def test_requirements_file_exists(self):
+        """
+        Tests the requirements file exists.
+        """
+        optional_dependencies_keys = read_optional_dependencies_keys()
+        # Assertion #1: The optional dependencies keys are set correctly.
+        assert len(self.optional_package_paths) + 1 == len(
+            optional_dependencies_keys
+        ), "Optional dependencies keys not set correctly."
+        for key in optional_dependencies_keys:
+            # Assertion #2: The key in the optional dependencies is in the optional package.
+            assert (
+                key in self.optional_package_paths if key != "essential" else True
+            ), f"Key {key} not found."
+            # Assertion #3: The requirement file path exists.
+            assert (
+                    len(read_requirements_paths(key)) != 0
+            ), f"Requirements file not found for {key}."
+
+    def test_requirements_alphabetical_order(self):
+        """
+        Tests the requirements are in alphabetical order.
+        """
+        requirements_dir = PWD / "requirements"
+        for path in requirements_dir.iterdir():
+            if path.is_file() and path.name.endswith(".txt"):
+                requirements = read_requirements_from_path(path)
+                # Assertion #1: The requirements are in alphabetical order.
+                assert requirements == sorted(
+                    requirements
+                ), f"Requirements in {path} are not in alphabetical order."
+
+    def test_essential_dependencies(self):
+        """
+        Tests the essential dependencies are in the requirements.
+        """
+        install_dependencies = [
+            dep
+            for deps in [
+                read_dependencies(PWD / package.removesuffix("*").replace(".", "/"))
+                for package in read_essential_packages()
+            ]
+            for dep in deps
+        ]
+        install_requirements = read_requirements("essential")
+        for dependency in set(install_dependencies):
+            # Assertion #1: The dependency is in the requirements.
+            assert any(
+                dependency == requirement for requirement in install_requirements
+            ), f"Dependency {dependency} not found in essentials requirements."
+
+    def test_optional_dependencies(self):
+        """
+        Tests the optional dependencies are in the requirements.
+        """
+        for key, path in self.optional_package_paths.items():
+            requirements = read_requirements(key)
+            dependencies = read_dependencies(path)
+            for dependency in set(dependencies):
+                # Assertion #1: The dependency is in the requirements.
+                assert any(
+                    dependency == requirement for requirement in requirements
+                ), f"Dependency {dependency} not found in {key}'s requirements."
---
+++ tests/dependencies/utils.py
@@ -0,0 +1,118 @@
+from io import StringIO
+from pathlib import Path
+from tomllib import load
+
+from pigar.core import RequirementsAnalyzer
+from pigar.parser import DEFAULT_GLOB_EXCLUDE_PATTERNS
+
+from football_data_puller.utils.constants import PWD
+
+
+def read_essential_packages() -> list[str]:
+    """
+    Reads the essential packages.
+    :return: The list of essential packages.
+    """
+    with open(PWD / "pyproject.toml", "rb") as fp:
+        data = load(fp)
+    return (
+        data.get("tool", {})
+        .get("setuptools", {})
+        .get("packages", {})
+        .get("find", {})
+        .get("include", [])
+    )
+
+
+def read_requirements_paths(key: str) -> list[Path]:
+    """
+    Reads the sub requirements paths.
+    :param key: The key of the sub requirements.
+    :return: The list of paths.
+    """
+    if key == "essential":
+        return [PWD / "requirements" / "essential.txt"]
+    else:
+        with open(PWD / "pyproject.toml", "rb") as fp:
+            data = load(fp)
+        return [
+            PWD / path
+            for path in data.get("tool", {})
+            .get("setuptools", {})
+            .get("dynamic", {})
+            .get("optional-dependencies", {})
+            .get(key, {})
+            .get("file", [])
+        ]
+
+
+def read_requirements_from_path(path: Path) -> list[str]:
+    """
+    Reads the requirements from the given path.
+    :param path: The path of the requirements file.
+    :return: The list of requirements.
+    """
+    with open(path, "r") as fp:
+        return [
+            line.split("==")[0]
+            for line in list(
+                filter(lambda x: not x.startswith("#"), fp.read().splitlines())
+            )
+        ]
+
+
+def read_requirements(key: str) -> list[str]:
+    """
+    Reads the requirements of the given key.
+    :param key: The key of the requirements.
+    :return: The list of requirements.
+    """
+    req_lines = list()
+    for sub_req_path in read_requirements_paths(key):
+        req_lines.extend(read_requirements_from_path(sub_req_path))
+    return req_lines
+
+
+def read_optional_dependencies_keys() -> list[str]:
+    """
+    Reads the sub requirements keys.
+    :return: The list of keys.
+    """
+    with open(PWD / "pyproject.toml", "rb") as fp:
+        data = load(fp)
+    return list(
+        data.get("tool", {})
+        .get("setuptools", {})
+        .get("dynamic", {})
+        .get("optional-dependencies", {})
+        .keys()
+    )
+
+
+def read_dependencies(path: Path) -> list[str]:
+    """
+    Reads the dependencies used by the given package directory.
+    :param path: The path of the package directory.
+    :return: The list of dependencies used in the directory.
+    """
+    analyzer = RequirementsAnalyzer(str(path))
+    buf = StringIO()
+    comparison_specifier = "=="
+    analyzer.analyze_requirements(
+        follow_symbolic_links=False,
+        ignores=DEFAULT_GLOB_EXCLUDE_PATTERNS,
+        visit_doc_str=False,
+    )
+    analyzer.write_requirements(
+        buf,
+        comparison_specifier=comparison_specifier,
+        with_banner=False,
+        with_ref_comments=False,
+        with_unknown_imports=False,
+    )
+    return [
+        dep.split(comparison_specifier)[0]
+        for dep in filter(
+            lambda x: comparison_specifier in x, buf.getvalue().split("\n")
+        )
+    ]
